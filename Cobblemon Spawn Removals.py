e8ecd55f94e34d35f45a64775c05aa7a7d81c7c0
Jamie Blanchard <priorsfarley@protonmail.com>
2024-08-02 13:02:26 +0100

    Creates the folder
    Fixes an error that occurs if the folder doesn't exist

--- Cobblemon Spawn Removals.py
+++ Cobblemon Spawn Removals.py
@@ -3,6 +3,9 @@ import os
 # Get the list of all files and directories
 path = "C:\\Users\prior\Downloads\CobblemonOriginalSpawnFiles"
 dir_list = os.listdir(path)
+newpath = r'SpawnFiles' 
+if not os.path.exists(newpath):
+    os.makedirs(newpath)
 for item in dir_list:
     file = open("SpawnFiles\\"+item,"w")
     file.write("{\n  \"enabled\": true,\n  \"neededInstalledMods\": [],\n  \"neededUninstalledMods\": [],\n  \"spawns\": []\n}")

a39ea64f0de854856f429b4dd7bc7914f0df86fd
toBlanch <priorsfarley@protonmail.com>
2026-02-08 15:23:07 +0000

    Fixed Path issues on Linux and removed hard coded path

--- Cobblemon Spawn Removals.py
+++ Cobblemon Spawn Removals.py
@@ -1,12 +1,17 @@
-# import OS module
 import os
-# Get the list of all files and directories
-path = "C:\\Users\prior\Downloads\CobblemonOriginalSpawnFiles"
+from pathlib import Path
+
+output_dir = Path('SpawnFiles')
+output_dir.mkdir(parents=True, exist_ok=True)
+
+path = ""
+if path == "":
+    path = input("Input the directory containing the original spawn files: ")
+
 dir_list = os.listdir(path)
-newpath = r'SpawnFiles' 
-if not os.path.exists(newpath):
-    os.makedirs(newpath)
 for item in dir_list:
-    file = open("SpawnFiles\\"+item,"w")
-    file.write("{\n  \"enabled\": true,\n  \"neededInstalledMods\": [],\n  \"neededUninstalledMods\": [],\n  \"spawns\": []\n}")
-    file.close()
+    file_path = output_dir / item
+    with file_path.open('w') as file:
+        file.write(
+            "{\n  \"enabled\": true,\n  \"neededInstalledMods\": [],\n  \"neededUninstalledMods\": [],\n  \"spawns\": []\n}"
+        )
